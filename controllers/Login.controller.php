b225c4b4ede1c915d919aba1ae5e8f09991e4b0c
38032718 <jeremiebasso@hotmail.fr>
2020-12-13 23:57:50 +0100

    Rename controller to match path /login rename variables names changed from sha1 to password_hash

---
+++ controllers/Login.controller.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once(DIR . "/controllers/Controller.php");
+
+class Login extends Controller
+{
+
+    public function index()
+    {
+        $this->load_model("User");
+        include_once ('cookieconnect.php');
+        if (isset($_POST['formconnexion']))
+        {
+            $mail = htmlspecialchars($_POST['mailconnect']);
+            $password = password_hash($_POST['mdpconnect']);
+            if (!empty($mail) and !empty($password))
+            {
+                $login_response = $this->User->authenticator($mail, $password);
+                if ($login_response == 1)
+                {
+                    if (isset($_POST['rememberme']))
+                    {
+                        setcookie('email', $mail, time()+365*24*3600, null, null, false, true);
+                        setcookie('password', $password, time()+365*24*3600, null, null, false, true);
+                    }
+                }
+                else
+                {
+                    $erreur = "Mauvais mail ou mot de passe !";
+                }
+            }
+            else
+            {
+                $erreur = "Tous les champs doivent être complétés !";
+            }
+        }
+        $this->render("connexion", ["erreur" => $erreur]);
+    }
+
+
+}
