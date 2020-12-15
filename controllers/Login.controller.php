4c429be77de44301022969e4da1eb0e42d2a3786
38032718 <jeremiebasso@hotmail.fr>
2020-12-15 02:16:38 +0100

    Improve remeber me features The client is no longer autoconnect, he got to press login button

--- controllers/Login.controller.php
+++ controllers/Login.controller.php
@@ -8,41 +8,48 @@ class Login extends Controller
 
     public function index()
     {
-        $this->render("connexion", []);
-        $this->try_auto_connect();
+        if (isset($_SESSION['NSS'])) {
+            header("Location: /profil");
+            exit();
+        }
+        //$this->try_auto_connect();
         $this->load_model("User");
         if (isset($_POST['mailconnect'])) {
             $this->post_login();
         }
+        $this->render("index", []);
     }
 
     private function post_login() {
         $mail = htmlspecialchars($_POST['mailconnect']);
-        $password = $_POST['mdpconnect'];
-        if (!empty($mail) and !empty($password)) {
+        $password = htmlspecialchars($_POST['mdpconnect']);
+        if ($mail != "" and $password != "") {
             $user = $this->User->authenticator($mail, $password);
             if ($this->check_user($user, $password)) {
-                header("Location: /users");
                 if (isset($_POST['rememberme'])) {
                     setcookie('auto_log_mail', $mail, time() + 365 * 24 * 3600, null, null, false, true);
                     setcookie('auto_log_password', $password, time() + 365 * 24 * 3600, null, null, false, true);
                 }
+                header("Location: /home");
+                exit();
             }
         } else {
-            throw new AlertException("Tout les champs doivent etre complété !");
+            throw new AlertException("Tout les champs doivent etre complété !", "BAD");
         }
     }
 
     private function try_auto_connect()
     {
-        if (!isset($_SESSION['id'])
-            and isset($_COOKIE['auto_log_mail'], $_COOKIE['auto_log_password'])
+        if (isset($_COOKIE['auto_log_mail'], $_COOKIE['auto_log_password'])
             and !empty($_COOKIE['auto_log_mail'])
             and !empty($_COOKIE['auto_log_password'])) {
 
             $this->load_model("User");
             $user = $this->User->authenticator($_COOKIE['auto_log_mail'], $_COOKIE['auto_log_password']);
-            $this->check_user($user, $_COOKIE['password']);
+            if($this->check_user($user, $_COOKIE['auto_log_password'])) {
+                header("Location: /home");
+                exit();
+            }
         }
     }
 
@@ -56,6 +63,7 @@ class Login extends Controller
             $_SESSION['nom'] = $user['Nom'];
             $_SESSION['prenom'] = $user['Prenom'];
             $_SESSION['mail'] = $user['Mail'];
+            $_SESSION['ROLE'] = $user['role_id'];
             return true;
         } else {
             throw new AlertException("Mauvais mot de passe");
