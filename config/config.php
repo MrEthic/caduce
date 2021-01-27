dc8cb0d332f0fea66ad93c3d2e10c9419a455a59
totodore <prevottheodore@gmail.com>
2021-01-27 17:45:43 +0100

    docker publish

--- config/config.php
+++ config/config.php
@@ -6,10 +6,10 @@ define('DB_PASSWORD', getenv('DB_PASSWORD') ? getenv('DB_PASSWORD') : "root" );
 define('DB_NAME', getenv('DB_NAME') ? getenv('DB_NAME') : "caducee" );
  */
 
-define("DBHOST", "localhost");
+define("DBHOST", "db");
 define("DBNAME", "caduce");
 define("DBUSER", "root");
-define("DBPSWD", "");
+define("DBPSWD", "caduce");
 
 
 
